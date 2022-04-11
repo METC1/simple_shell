d1b3e9f9898155a4b724357d288b7eb3e9f8c416
METC <metc_ck1@hotmail.com>
2022-04-11 16:10:04 -0700

    Implementing env

--- shell_main.c
+++ shell_main.c
@@ -10,15 +10,17 @@
  * @argc: argument count
  * Return: 0 if success.
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], *envp[])
 {
 	char sign = 0;
 	char **array = NULL;
+	char **envarray = NULL;
 
 	if (argc == 1)
 	{
 		do
 		{
+			envarray = builtin_env(envpr);
 			_putchar('$');
 			_putchar(' ');
 			array = line_to_array(argv[0]);
